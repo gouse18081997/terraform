128ebe1d17c02591bc30335b01358647e3f6c0ac
gouse18081997 <gouse18081997@gmail.com>
2025-07-09 10:12:26 +0530

    main.tf changes

--- Day-2-Introduction/main.tf
+++ Day-2-Introduction/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
-  ami = "ami-05ffe3c48a9991133"
-  instance_type = "t2.micro"
+  ami = var.ami_id
+  instance_type = var.instance_type
   tags = {
     Name = "day-2"
   }
